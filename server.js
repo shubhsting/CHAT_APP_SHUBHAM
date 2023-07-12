e5916795cb37b12b12c94cf6cf359b41948dba44
Shubham Singh <shpsingh@amazon.com>
2023-07-12 10:16:48 +0530

    added chat backend

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { loginUser, signupUser } = require("./controller/userController");
+const cors = require("cors")
+const app = express()
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+app.use(cors())
+app.use(express.json())
+
+const server = require('http').createServer(app);
+const io = require("socket.io")(server,{
+    cors: {
+        origin: '*'
+    }
+});
+
+io.on("connection", (socket)=>{
+    const auth_token = socket.handshake.query.token;
+    const decoded = jwt.verify(auth_token, process.env.JWT_SECRET_KEY);  
+    socket.join(123456789)  
+    socket.to(123456789).emit("new_user_joined", {
+        message: `${decoded.firstName} ${decoded.lastName} has joined`
+    })
+
+    socket.on("user_message", (response)=> {
+        console.log(`message received on server ${response.message}`)
+        socket.to(123456789).emit("new_message_sent_by_someone", {
+            message: response.message,
+            owner: jwt.verify(response.auth_token, process.env.JWT_SECRET_KEY)
+        })
+    })
+})
+
+
+// emit send the event to all the participants except the person who triggered backend.
+app.post("/login", loginUser)
+app.post("/signup", signupUser)
+const port = process.env.PORT || 5000;
+server.listen(port, ()=>{
+    console.log("server started")
+})
+
+
+// socket.emit(event_name, data)
+// socket.on(event_name, (data)=>{
+// console.log(data)
+// })
